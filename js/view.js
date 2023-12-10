0de2e375cbec8d8f9e49fa77d62050b2f4598c96
enno <enrico.harder@gmail.com>
2023-12-10 05:27:21 +0100

    Update start screen and start button (generated by blackbox.ai )

--- js/view.js
+++ js/view.js
@@ -1,4 +1,10 @@
 document.getElementById('startGameButton').addEventListener('click', function() {
-    document.getElementById('startScreen').style.display = 'none';
+    document.getElementById('startScreen').style.display = 'block';
     document.getElementById('gameScreen').style.display = 'block';
 });
+
+    // JavaScript-Code für das Startmenü
+    function showGameScreen() {
+        // Hier können Sie den Code hinzufügen, um das Spiel zu starten
+        alert("Spiel wird gestartet...");
+      }
